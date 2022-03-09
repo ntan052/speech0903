9312dfd664702b341877db5e4ba1001f7d762f15
ntan052 <99786883+ntan052@users.noreply.github.com>
2022-03-09 15:53:57 +0800

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+from flask import Flask
+
+
+# In[42]:
+
+
+app = Flask(__name__)
+
+
+# In[43]:
+
+
+from flask import request, render_template
+
+
+# In[44]:
+
+
+from werkzeug.utils import secure_filename
+import speech_recognition as sr
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files["file"]
+        #print("file received")
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save("static/" + filename)
+        a = sr.AudioFile("static/" + filename)
+        with a as source:
+            a = sr.Recognizer().record(source)
+        s = sr.Recognizer().recognize_google(a)
+        return(render_template("index.html", result = s))
+    else:
+        return(render_template("index.html", result = "2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
